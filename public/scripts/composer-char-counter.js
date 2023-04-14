802fb736245bfe28d01f5ec60d210657d7df6573
Mohammad Razaul Islam <shovon231@gmail.com>
2023-04-14 22:16:03 +0000

    clean and removong dead code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,18 +2,14 @@ $(document).ready(function () {
   // --- our code goes here ---
   const counter = 140;
   $("#tweet-text").on("input", function () {
-    //console.log(counter - $(this).$.trim(val()).length);
     let textVal = counter - $(this).val().length;
-    //$(this.document).text(textVal);
-    let selectOutput = $("#tweet-text, .counter"); //.parent().children("div").children("output");
-
+    let selectOutput = $("#tweet-text, .counter");
     $(selectOutput).text(textVal);
     if (textVal <= 0) {
       $(selectOutput).addClass("red");
     } else {
       $(selectOutput).removeClass("red");
+      $(".displayError").css("visibility", "hidden");
     }
   });
 });
-
-
